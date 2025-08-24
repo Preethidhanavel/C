0b96e7a51823a771beaca08bb2103ad18e25073e
Preethi D <27preethid@gmail.com>
2025-08-24 14:18:49 +0530

    C

--- C_Worksheet/P03_atomic_toggle.c
+++ C_Worksheet/P03_atomic_toggle.c
@@ -8,13 +8,13 @@ Problem Statement:
 #include <pthread.h>
 #include <unistd.h>
 
-// Simulated MMIO register (volatile because hardware registers change externally)
+// Simulated MMIO register 
 volatile uint32_t GPIO_REG = 0;
 
 // Bit to toggle
 #define GPIO_BIT (1u << 3)   // Bit 3
 
-// Approach 1: Use atomic operations (portable in C11 and later)
+//atomic operation
 void gpio_toggle_atomic(void) {
     // XOR bit atomically
     atomic_fetch_xor((_Atomic uint32_t *)&GPIO_REG, GPIO_BIT);
@@ -45,3 +45,4 @@ int main(void) {
     printf("Final GPIO_REG = 0x%08X\n", GPIO_REG);
     return 0;
 }
+
